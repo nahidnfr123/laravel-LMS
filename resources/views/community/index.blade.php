3b99316498817292e46437ddb0f1db00ee7fd49a
nahidnfr123 <nahidferdous240@gmail.com>
2022-10-18 23:46:33 +0600

    Assignment and free course enroll

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -20,45 +20,49 @@
                         <a href="{{route('community_post.create')}}" class="btn btn-primary">Post on Community</a>
                     </div>
                 @endif
-                @foreach($communityPosts as $post)
-                    <div class="col-lg-6 col-md-8 col-12 mb-4">
-                        <div class="course-item">
+                @if(count($communityPosts))
+                    @foreach($communityPosts as $post)
+                        <div class="col-lg-6 col-md-8 col-12 mb-4">
+                            <div class="course-item">
 
-                            <div class="course-br">
-                                <div class="d-flex" style="column-gap: 20px">
-                                    <img src="{{$post->user->photo ?? 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-1024.png'}}" alt="" height="60px" width="60px">
-                                    <div>
+                                <div class="course-br">
+                                    <div class="d-flex" style="column-gap: 20px">
+                                        <img src="{{$post->user->photo ?? 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-1024.png'}}" alt="" height="60px" width="60px">
                                         <div>
-                                            <strong>{!! $post->user->name !!}</strong>
+                                            <div>
+                                                <strong>{!! $post->user->name !!}</strong>
+                                            </div>
+                                            @if(auth()->check() && $post->user->id === auth()->id())
+                                                <form action="{{ route('community_post.destroy', $post->id) }}" method="POST">
+                                                    {{ csrf_field() }}
+                                                    {{ method_field('DELETE') }}
+                                                    <a href="{{route('community_post.edit', $post->id)}}" class="btn btn-sm btn-warning rounded">
+                                                        <i class="fa fa-pencil"></i>
+                                                    </a>
+                                                    <button type="submit" class="btn btn-xs btn-danger rounded-lg"><i class="fa fa-trash"></i></button>
+                                                </form>
+                                            @endif
                                         </div>
-                                        @if(auth()->check() && $post->user->id === auth()->id())
-                                            <form action="{{ route('community_post.destroy', $post->id) }}" method="POST">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-                                                <a href="{{route('community_post.edit', $post->id)}}" class="btn btn-sm btn-warning rounded">
-                                                    <i class="fa fa-pencil"></i>
-                                                </a>
-                                                <button type="submit" class="btn btn-xs btn-danger rounded-lg"><i class="fa fa-trash"></i></button>
-                                            </form>
-                                        @endif
+                                    </div>
+                                    <div><h2>{{ $post->title }}</h2></div>
+                                    <div class="image-blog">
+                                        <img src="{{$post->photo}}" alt="" class="img-fluid" style="max-height: 300px">
+                                    </div>
+                                    <div class="mt-2 d-flex justify-content-between align-center">
+                                        <div>Published at: <strong>{{$post->publish_at}}</strong></div>
+
+                                        <a href="{{route('community_post.show', $post->id)}}" class="btn btn-sm btn-warning">Read more</a>
+                                    </div>
+                                    <div class="mt-1" style="max-height: 200px; overflow: hidden">
+                                        {!! $post->description !!}
                                     </div>
                                 </div>
-                                <div><h2>{{ $post->title }}</h2></div>
-                                <div class="image-blog">
-                                    <img src="{{$post->photo}}" alt="" class="img-fluid" style="max-height: 300px">
-                                </div>
-                                <div class="mt-2 d-flex justify-content-between align-center">
-                                    <div>Published at: <strong>{{$post->publish_at}}</strong></div>
-
-                                    <a href="{{route('community_post.show', $post->id)}}" class="btn btn-sm btn-warning">Read more</a>
-                                </div>
-                                <div class="mt-1" style="max-height: 200px; overflow: hidden">
-                                    {!! $post->description !!}
-                                </div>
                             </div>
-                        </div>
-                    </div><!-- end col -->
-                @endforeach
+                        </div><!-- end col -->
+                    @endforeach
+                @else
+                    <div class="my-2 alert alert-info">No content found</div>
+                @endif
             </div><!-- end row -->
         </div><!-- end container -->
     </div><!-- end section -->
--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -15,57 +15,61 @@
         <div class="container">
 
             <div class="row">
-                @foreach($courses as $course)
-                    <div class="col-lg-4 col-md-6 col-12">
-                        <div class="course-item">
-                            <div class="image-blog">
-                                <img src="{{$course->photo}}" alt="" class="img-fluid" style="height: 240px!important;">
-                            </div>
-                            <div class="course-br">
-                                <div class="course-title">
-                                    <h2><a href="{{ route('home.course', $course->id) }}" title="">{{$course->title}}</a></h2>
+                @if(count($courses))
+                    @foreach($courses as $course)
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="course-item">
+                                <div class="image-blog">
+                                    <img src="{{$course->photo}}" alt="" class="img-fluid" style="height: 240px!important;">
                                 </div>
-                                <div class="course-desc">
-                                    <p></p>
-                                </div>
-                                <div class="course-rating">
-                                    <strong class="mr-2">{{$course->rating}}</strong>
-                                    <strong>
-                                        @for ($i = 0; $i < 5; ++$i)
-                                            <i class="fa fa-star{{ $course->rating <= $i ? '-o' : ''}} '" aria-hidden="true"></i>
-                                        @endfor
-                                    </strong>
-                                </div>
+                                <div class="course-br">
+                                    <div class="course-title">
+                                        <h2><a href="{{ route('home.course', $course->id) }}" title="">{{$course->title}}</a></h2>
+                                    </div>
+                                    <div class="course-desc">
+                                        <p></p>
+                                    </div>
+                                    <div class="course-rating">
+                                        <strong class="mr-2">{{$course->rating}}</strong>
+                                        <strong>
+                                            @for ($i = 0; $i < 5; ++$i)
+                                                <i class="fa fa-star{{ $course->rating <= $i ? '-o' : ''}} '" aria-hidden="true"></i>
+                                            @endfor
+                                        </strong>
+                                    </div>
 
-                                @if(auth()->check() && $course->subscription_status)
-                                    <p class="text-success">
-                                        <strong>{{$course->subscription_status ? 'Active' : ''}}</strong>
-                                    </p>
-                                @else
-                                    @if($course->discounted_price)
-                                        <p>Tk.
-                                            <span style="text-decoration: line-through;">
+                                    @if(auth()->check() && $course->subscription_status)
+                                        <p class="text-success">
+                                            <strong>{{$course->subscription_status ? 'Active' : ''}}</strong>
+                                        </p>
+                                    @else
+                                        @if($course->discounted_price)
+                                            <p>Tk.
+                                                <span style="text-decoration: line-through;">
                                        {{ $course->price}}
                                     </span>
-                                            <strong>{{$course->discounted_price}}</strong>
-                                        </p>
-                                    @elseif($course->price)
-                                        <p>Tk. <strong>{{$course->discounted_price ?? $course->price}}</strong></p>
-                                    @else
-                                        <p><strong class="text-success">Free</strong></p>
+                                                <strong>{{$course->discounted_price}}</strong>
+                                            </p>
+                                        @elseif($course->price)
+                                            <p>Tk. <strong>{{$course->discounted_price ?? $course->price}}</strong></p>
+                                        @else
+                                            <p><strong class="text-success">Free</strong></p>
+                                        @endif
                                     @endif
-                                @endif
+                                </div>
+                                <div class="course-meta-bot">
+                                    <ul>
+                                        <li><i class="fa fa-calendar" aria-hidden="true"></i> 6 Month</li>
+                                        <li><i class="fa fa-users" aria-hidden="true"></i> {{ count($course->users) }} Student</li>
+                                        <li><i class="fa fa-book" aria-hidden="true"></i> 7 Books</li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div class="course-meta-bot">
-                                <ul>
-                                    <li><i class="fa fa-calendar" aria-hidden="true"></i> 6 Month</li>
-                                    <li><i class="fa fa-users" aria-hidden="true"></i> {{ count($course->users) }} Student</li>
-                                    <li><i class="fa fa-book" aria-hidden="true"></i> 7 Books</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div><!-- end col -->
-                @endforeach
+                        </div><!-- end col -->
+                    @endforeach
+                @else
+                    <div class="my-2 alert alert-info">No content found</div>
+                @endif
             </div><!-- end row -->
         </div><!-- end container -->
     </div><!-- end section -->
